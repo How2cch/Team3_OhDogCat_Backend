8bc376eb37850811ee0fda979612dcb073ed6ed5
howard.chen <howardchen1617@gmail.com>
2022-09-29 19:23:12 +0800

    [pull] 0929 19:23

--- models/userAuth.js
+++ models/userAuth.js
@@ -2,10 +2,7 @@ const pool = require('../utils/db');
 
 const isAccountExist = async (user) => {
   try {
-    let [userResult] = await pool.execute(
-      'SELECT email, social_name FROM user WHERE email = ? OR social_name = ?',
-      [user.email, user.socialName]
-    );
+    let [userResult] = await pool.execute('SELECT email, social_name FROM user WHERE email = ? OR social_name = ?', [user.email, user.socialName]);
     return userResult;
   } catch (error) {
     console.error(error);
@@ -14,10 +11,7 @@ const isAccountExist = async (user) => {
 
 const isLoginUserExist = async (user) => {
   try {
-    let [userResult] = await pool.execute(
-      'SELECT id, password, email, name, social_name, photo FROM user WHERE email = ? AND valid = 1 ',
-      [user.email]
-    );
+    let [userResult] = await pool.execute('SELECT id, password, email, name, social_name, photo FROM user WHERE email = ? AND valid = 1 ', [user.email]);
     return userResult;
   } catch (error) {
     console.error(error);
@@ -26,16 +20,13 @@ const isLoginUserExist = async (user) => {
 
 const insertUser = async (user) => {
   try {
-    const [result] = await pool.execute(
-      'INSERT INTO user (email, password, social_name, photo, create_time) VALUE (?, ?, ?, ?, ?)',
-      [
-        user.email,
-        user.password,
-        user.social_name,
-        user.photo,
-        user.create_time,
-      ]
-    );
+    const [result] = await pool.execute('INSERT INTO user (email, password, social_name, photo, create_time) VALUE (?, ?, ?, ?, ?)', [
+      user.email,
+      user.password,
+      user.social_name,
+      user.photo,
+      user.create_time,
+    ]);
     return result;
   } catch (error) {
     console.error(error);
